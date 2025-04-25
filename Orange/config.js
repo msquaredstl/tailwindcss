aa75c5b774afdfb3233389a2b5d89f9dc27221be
msquaredstl <msquaredstl@gmail.com>
2025-04-24 23:50:22 -0500

    Added Orange project

---
+++ Orange/config.js
@@ -0,0 +1,19 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+            pale_orange: '#ffd9a6',
+            light_orange: '#fbb03b',
+            orange: '#f7931e',
+            },
+            fontFamily: {
+                sans: ['Josefin Sans', 'sans-serif'],
+                title: ['Playfair Display SC', 'serif']
+            },
+            backgroundImage: {
+                bg_desktop: "url('bg-desktop.png')",
+                bg_mobile: "url('bg-mobile.png')"
+            }
+        }
+    }
+}
